42770b28f90ed259ff04fa19ec2d221cfcfa295a
IlyaYazev <163823253+IlyaYazev@users.noreply.github.com>
2024-03-22 15:17:21 +0300

    Add files via upload

---
+++ Homework12.py
@@ -0,0 +1,18 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print (1, 'строка', True)
+    print ('строка', True)
+    print(True)
+    print()
+print_params()
+print_params(b = 25) # повторяет строку 6 без учета содержимого
+print_params(c = [1,2,3]) # # повторяет строку 6 без учета содержимого
+
+
+
+
+
+
+
+
+
+
