f24a7ae90ddcf9d311ac33ccb8369bf979f0d012
IlyaYazev <163823253+IlyaYazev@users.noreply.github.com>
2024-03-25 15:54:53 +0300

    Update Homework12.py

--- Homework12.py
+++ Homework12.py
@@ -1,11 +1,24 @@
-def print_params(a = 1, b = 'строка', c = True):
-    print (1, 'строка', True)
-    print ('строка', True)
-    print(True)
+def print_params(a, b, c):
+    print (a, b, c)
+    print (b, c)
+    print(c)
     print()
-print_params()
-print_params(b = 25) # повторяет строку 6 без учета содержимого
-print_params(c = [1,2,3]) # # повторяет строку 6 без учета содержимого
+
+list_ = [1, 'строка', True]
+print_params(*list_)
+
+list_2 = [1, 25, [1, 2, 3]]
+print_params(*list_2)
+
+values_list = ["абзац", False, 5]
+values_dict = {'a': "Цифра", 'b': 90, 'c': "Правда"}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = ["текст", 100]
+print_params(*values_list_2, 42)
+
 
 
 
