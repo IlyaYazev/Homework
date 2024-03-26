c7a2226cb30aaf6cffc0188c47b957b7ce944432
IlyaYazev <163823253+IlyaYazev@users.noreply.github.com>
2024-03-26 14:15:51 +0300

    Update homework10.py

--- homework10.py
+++ homework10.py
@@ -2,6 +2,13 @@ def print_params(*arg):
     print(2*arg)
 print_params("I believe in miracles", 666)
 
+def  print_params(num):
+        print(num)
+        print(num)
+
+print_params('Йа аргумент!')
+print_params(42)
+
 
 
 
