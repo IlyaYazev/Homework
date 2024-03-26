8dd4715e398ffae5fb0f48994036df0cd99fc587
IlyaYazev <163823253+IlyaYazev@users.noreply.github.com>
2024-03-26 15:12:28 +0300

    Add files via upload
    2023/10/09 00:00|Самостоятельная работа по уроку "Произвольное число параметров и HTML_DOM"
    Самостоятельная работа по уроку "Произвольное число параметров и HTML_DOM"
    Для работы над домашнем заданием:
    1. Создайте новый проект в PyCharm
    
    Ваша задача в файле main.py:
    Создайте новую функцию def test... с произвольным числом параметров разного типа, функция должна распечатывать эти параметры внутри своего тела
    Создайте рекурсивную функцию, которая будет считать факториал от числа n, n - передается в параметре
    В ответ прикрепите получившийся файл main.py
    Урок "Функции и произвольное число параметров"
    Урок "Рекурсия и HTML_DOM"

---
+++ Homework13.py
@@ -0,0 +1,12 @@
+def test(*args):
+    print(*args)
+test("I believe in miracles", 666, 'Пример')
+
+def factorial (n):#Создайте рекурсивную функцию, которая будет считать факториал от числа n, n - передается в параметре
+    if n == 1:
+        return 1
+    factorial_n_minus_1 = factorial(n = n - 1)
+    return  n * factorial_n_minus_1
+
+print(factorial(50))
+
