5639507de33430303e953e83bcba9259ae736f1d
IlyaYazev <163823253+IlyaYazev@users.noreply.github.com>
2024-03-26 19:24:13 +0300

    Update Homework13.py
    но стоит добавить базовый случай для n == 0, так как факториал от нуля по определению равен 1. Нужно будет объединить случаи n==1 и n==0.

--- Homework13.py
+++ Homework13.py
@@ -1,12 +1,24 @@
+#def test(*args):
+#    print(*args)
+#test("I believe in miracles", 666, 'Пример')
+
+#def factorial (n):#Создайте рекурсивную функцию, которая будет считать факториал от числа n, n - передается в параметре
+#    if n == 1:
+#        return 1
+#    factorial_n_minus_1 = factorial(n = n - 1)
+#    return  n * factorial_n_minus_1
+
+#print(factorial(50))
+
+# Вариант 2 (устранение замечаний)
 def test(*args):
     print(*args)
 test("I believe in miracles", 666, 'Пример')
 
 def factorial (n):#Создайте рекурсивную функцию, которая будет считать факториал от числа n, n - передается в параметре
-    if n == 1:
+    if n% 1 == 0: #но стоит добавить базовый случай для n == 0, так как факториал от нуля по определению равен 1. Нужно будет объединить случаи n==1 и n==0.
         return 1
     factorial_n_minus_1 = factorial(n = n - 1)
     return  n * factorial_n_minus_1
 
 print(factorial(50))
-
