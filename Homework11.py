25e5fa2c6f940507e5e73d88ba5ebacd4bd24553
IlyaYazev <163823253+IlyaYazev@users.noreply.github.com>
2024-03-27 12:43:58 +0300

    Update Homework11.py

--- Homework11.py
+++ Homework11.py
@@ -4,9 +4,19 @@ def test():
     print(a, b)
 test()
 
-def test2 (a, b, c):    #I learn, Python, Three weeks
-    print("I learn", "Python", "Three weeks")
-test2("", "", "")
+def test2 (a = "I learn", b = "Python", c = "Three weeks"):    #I learn, Python, Three weeks
+    print(a, b, c)
+test2()
 
+def test3 (Имя, возраст):
+    print(f"Имя: {Имя}")
+    print(f"Возраст: {возраст}")
+test3("Петр", 40)
 
+def sum(*numbers):
+    result = 0
+    for n in numbers:
+        result += n
+        print(f"sum = {result}")
 
+sum(1, 2, 3, 4, 5, 6, 7, 8, 9)
