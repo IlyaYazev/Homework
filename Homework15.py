6f14902f8dd82d6d2daae6eb996929ae5d11884b
IlyaYazev <163823253+IlyaYazev@users.noreply.github.com>
2024-04-09 11:59:51 +0300

    Add files via upload
    Домашнее задание по уроку "Пространство имен"
    
    1. Создайте новый проект в PyCharm
    2. Запустите созданный проект
    Ваша задача:
    1. Создайте новую функцию def test_function
    2. Создайте другую функцию внутри функции inner_function, функция должна печатать значение "Я в области видимости функции test_function"
    3. Вызовите функцию inner_function внутри функции test_function
    4. Попробуйте вызывать inner_function вне функции test_function и посмотрите на результат выполнения программы
    Полученный код напишите в ответ к домашнему заданию

---
+++ Homework15.py
@@ -0,0 +1,8 @@
+def test_function(): # Создайте новую функцию def test_function
+    def inner_function(): # Создайте другую функцию внутри функции inner_function
+        print("Я в области видимости функции test_function") #функция должна печатать значение "Я в области видимости функции test_function"
+    inner_function() # Вызовите функцию inner_function внутри функции test_function
+
+test_function()
+#если вызываем inner_funtion() то NameError: name 'inner_funtion' is not defined
+
