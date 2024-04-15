6fa9d8e99a97225180d30e530f7debd6e5ef8920
IlyaYazev <163823253+IlyaYazev@users.noreply.github.com>
2024-04-15 15:22:06 +0300

    Add files via upload

---
+++ Homework17.py
@@ -0,0 +1,20 @@
+#1. Создайте новый класс House
+#2. Создайте инициализатор для класса House, который будет задавать атрибут этажности self.numberOfFloors = 0
+#3. Создайте метод setNewNumberOfFloors(floors),
+#который будет изменять атрибут numberOfFloors на параметр floors и выводить в консоль numberOfFloors
+
+class House: #1
+    def __init__(self):
+        self.numberOfFloors = 0 #2
+
+    def NewNumberOfFloors(self, floors): #3
+         print(numberOfFloors)
+
+house = House()
+var = house.numberOfFloors  ### нужна подсказка, в верном ли напрвлении пишу и куда дальше?
+
+
+
+        
+
+
