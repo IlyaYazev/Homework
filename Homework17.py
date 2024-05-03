7edce3cb6f0049e93e43dcbcefccfe3b489fdf3a
IlyaYazev <163823253+IlyaYazev@users.noreply.github.com>
2024-05-03 14:50:09 +0300

    Update Homework17.py

--- Homework17.py
+++ Homework17.py
@@ -7,14 +7,9 @@ class House: #1
     def __init__(self):
         self.numberOfFloors = 0 #2
 
-    def NewNumberOfFloors(self, floors): #3
-         print(numberOfFloors)
+    def setNewNumberOfFloors(self, floors): #3
+         self.numberOfFloors = floors
+         print(floors)
 
 house = House()
 var = house.numberOfFloors  ### нужна подсказка, в верном ли напрвлении пишу и куда дальше?
-
-
-
-        
-
-
